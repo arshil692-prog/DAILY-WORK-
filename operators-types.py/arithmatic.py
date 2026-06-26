bacff3da824b15c2f80710e8ecd0fc2fe5637333
arshil692-prog <muktars692@gmail.com>
2026-06-26 14:38:47 +0530

    types with example

---
+++ operators-types.py/arithmatic.py
@@ -0,0 +1,38 @@
+a = 15
+b = 4
+
+print("----------------------------")
+print("Arithmetic Operators: ")
+print("----------------------------")
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b) 
+
+#comparision operators
+
+print("----------------------------")
+print("Comparison Operators: ") 
+print("----------------------------")
+
+
+print(5 == 5)
+print(10 != 20)
+print(15 > 10)
+print(5 < 10)
+print(15 >= 15)
+print(5 <= 10)
+
+
+print("----------------------------")
+print("Logical Operators: ")
+print("----------------------------")
+
+
+print(5 > 3 and 10 > 5)
+print(5 > 3 or 10 < 5)
+print(not(5 > 3 and 10 > 5))

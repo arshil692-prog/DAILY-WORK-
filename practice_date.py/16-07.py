bf69ffc80fabba2b7e6425dde37012aa66281fa7
arshil692-prog <muktars692@gmail.com>
2026-07-16 10:47:52 +0530

    assignments

---
+++ practice_date.py/16-07.py
@@ -0,0 +1,28 @@
+#section A 
+l1 = []
+print(l1)
+
+l2 = ["arshil","rohit","rohan","rushi","sekhar"]
+print(l2)
+
+marks = [99,23,77,89,57,]
+print(marks)
+
+intro = ["arshil","ugaon",20, "%87"]
+print (intro)
+
+num = [[1,2,3],[4,5,6,],[7,8,9]]
+print( num [0:3])
+
+num1 = [
+    [12,34,56],
+    [32,44,88]
+    ]
+print (num1[0:2:2])
+
+#using lenth method
+print(len(l1))
+
+programmers = ["python","java","c++","c+","javascript"]
+print("favorite programming languages",programmers)
+

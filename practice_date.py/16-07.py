6b75c22c9ab1d28b4232242ce3a032e7b1c6bebc
arshil692-prog <muktars692@gmail.com>
2026-07-16 12:50:25 +0530

    15-07 assignment

--- practice_date.py/16-07.py
+++ practice_date.py/16-07.py
@@ -14,10 +14,7 @@ print (intro)
 num = [[1,2,3],[4,5,6,],[7,8,9]]
 print( num [0:3])
 
-num1 = [
-    [12,34,56],
-    [32,44,88]
-    ]
+num1 = [[1,2,2],[3,4,5]]
 print (num1[0:2:2])
 
 #using lenth method
@@ -25,4 +22,45 @@ print(len(l1))
 
 programmers = ["python","java","c++","c+","javascript"]
 print("favorite programming languages",programmers)
+
+#section B
+
+city_name = ["nagpur","bombay","puna","nashik","niphad"]
+print(city_name[2])
+
+print(city_name[-1])
+
+print(city_name[0:5:3])
+
+print(city_name[::-1])
+
+print (city_name[::2])
+
+print(city_name[0:6:2])
+
+
+name = [["hello"],["hiii"],["heyyy"],["whtasapp"],["yepp"]]
+print(name[2])
+
+#diffrence between (=) and copy() 
+#1////  (=)
+
+list1 = [1,2,3]
+list2 = list1
+
+list2.append(4)
+
+print (list1,list2)
+print(id(l1))
+print(id(l2))
+#2/// copy()
+
+l1 = [1,2,3]
+l2 = l1.copy()
+
+l2.append(4)
+print(l1,l2)
+print(id(l1))
+print(id(l2))
+
 
